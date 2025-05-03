65cb744d9551d1f6831cfc211dc71bc2eea7f01b
Mikol Faro <mikol.faro@proton.me>
2025-05-03 14:53:02 +0200

    Create database using sqlx

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,36 +1,49 @@
-use tauri_plugin_sql::{Migration, MigrationKind};
+use sqlx::{migrate::MigrateDatabase, sqlite::SqlitePoolOptions, Sqlite, SqlitePool};
+use tauri::{App, Manager};
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+type Db = SqlitePool;
+struct AppState { db: Db }
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+async fn setup_db(app: &App) -> Db {
+    let mut path = app.path().app_data_dir().expect("Failed to get data_dir");
+    if let Err(err) = std::fs::create_dir_all(path.clone()) {
+        panic!("Error creating directory {}", err);
+    }
+
+    path.push("app.db");
+
+    Sqlite::create_database(format!("sqlite:{}", path.to_str().expect("Path should be something")).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let db = SqlitePoolOptions::new()
+        .connect(path.to_str().unwrap())
+        .await
+        .unwrap();
+
+    sqlx::migrate!("./migrations").run(&db).await.unwrap();
+
+    db
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = vec![Migration {
-        version: 1,
-        description: "create_products_table",
-        sql: "
-                CREATE TABLE IF NOT EXISTS products (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL UNIQUE,
-                    price REAL NOT NULL CHECK(price >= 0),
-                    category TEXT NOT NULL
-                );
-                ",
-        kind: MigrationKind::Up,
-    }];
-
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .plugin(
-            tauri_plugin_sql::Builder::default()
-                .add_migrations("sqlite:app.db", migrations)
-                .build(),
-        )
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            tauri::async_runtime::block_on(async move {
+                let db = setup_db(&app).await;
+                app.manage(AppState { db });
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
