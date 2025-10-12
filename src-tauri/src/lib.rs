2219e363bba8f5012a7ab8c2142373ef7f99b6aa
Mikol Faro <mikol.faro@proton.me>
2025-10-12 10:10:08 +0200

    Ignore install task

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,7 @@ use escpos::{
     printer::Printer,
     utils::{DebugMode, Protocol},
 };
-use log::{debug, info};
+use log::{info};
 use printing::{print_tickets, PrintingLayout};
 use rusb::{Context, DeviceList};
 use serde::{Deserialize, Serialize};
